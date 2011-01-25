7d53025710ec7f9534605e7e67496512cff4ac0e
feder <federliang@gmail.com>
2011-01-26 00:00:58 +0800

    second commit

---
+++ SecondLevelViewController.m
@@ -0,0 +1,20 @@
+//
+//  SecondLevelViewController.m
+//  FakeSMS
+//
+//  Created by Feder on 10-5-1.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "SecondLevelViewController.h"
+
+
+
+
+@implementation SecondLevelViewController
+@synthesize	rowImage;
+//@synthesize bubbleImage;
+//@synthesize	textfield;
+
+
+@end
